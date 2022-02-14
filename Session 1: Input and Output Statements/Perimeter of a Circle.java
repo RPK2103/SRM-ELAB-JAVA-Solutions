95ba333de5cce715409519086e473924db727694
Kaviyashre Ragupathy <60558428+RPK2103@users.noreply.github.com>
2022-02-14 23:09:53 +0530

    Create Perimeter of a Circle.java
    Java Program to calculate Perimeter of the circle using radius.

---
+++ Session 1: Input and Output Statements/Perimeter of a Circle.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.*;
+public class TestClass {
+	 public static void main(String[] args) { 
+		Scanner sc= new Scanner(System.in);
+       	int r=sc.nextInt();
+       	System.out.println(String.format("%.2f",(2*r*3.14)));
+	}
+}
